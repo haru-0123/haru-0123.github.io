171655ea064ef95fac4c30e627adaf292ef0d127
haru-0123 <151296296+haru-0123@users.noreply.github.com>
2024-01-24 16:46:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 <script>
-// スクロールイベントを検知する関数
 function checkScroll() {
     document.querySelectorAll('.inner').forEach(el => {
         const elementTop = el.getBoundingClientRect().top;
@@ -8,11 +7,12 @@ function checkScroll() {
         // 要素が表示領域に入ったかを判断
         if (elementTop < viewportHeight - 100) {
             el.classList.add('visible');
-        } else {
-            el.classList.remove('visible');
         }
     });
 }
+
+// 初回表示時にも機能するように関数を実行
+checkScroll();
 
 // スクロール時に関数を実行
 window.addEventListener('scroll', checkScroll);
